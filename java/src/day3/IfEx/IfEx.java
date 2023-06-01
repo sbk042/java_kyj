75663c7dd6bfe2a649bd1a4451be968d5b83188c
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-01 14:57:56 +0900

    자바 : if 조건문 예시 내가 푼 거

---
+++ java/src/day3/IfEx/IfEx.java
@@ -0,0 +1,21 @@
+package day3.IfEx;
+
+public class IfEx {
+
+	public static void main(String[] args) {
+		// 정수 num가 0이면 0입니다 라고 출력하는 코드를 작성하세요.
+		// 정수 num가 0이다
+		// 정수 num가 0과 같다.
+		int num = 10; // num이 얼마인가에 따라 결과가 달라진다.
+		if(num == 0) {
+			// 0입니다 라고 출력;
+			System.out.println("0입니다.");
+		}
+        // 정수 num가 0이 아니면 num is not 0라고 출력하는 코드를 작성하세요.
+		// num가 0과 같지 않다.
+		if(num != 0 ) {
+			System.out.println(num + " is not 0 ");
+		}
+	}
+ 
+}
