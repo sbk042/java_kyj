0adacbd9865d1e306ffeb5b1ea31b3102b579111
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-05 15:03:15 +0900

    day5.homework

---
+++ java/src/day5/homework/UpDownEx.java
@@ -0,0 +1,19 @@
+package day5.homework;
+
+public class UpDownEx {
+
+	public static void main(String[] args) {
+		/* UpDown게임
+		 * 1~100사이의 랜덤한 정수를 생성하고, 이 정수를 맞추는 게임을 구현하세요.
+		 * 랜덤한 수 : 45
+		 * input : 50
+		 * Down!
+		 * input : 40
+		 * up!
+		 * input : 45
+		 * Good!
+		 */
+
+	}
+
+}
