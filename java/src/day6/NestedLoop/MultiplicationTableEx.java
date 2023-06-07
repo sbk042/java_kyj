2c774b245cc68b66e39efd83108ee8ad021ec46b
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-07 14:46:06 +0900

    자바 : for 중첩반복문 예제

---
+++ java/src/day6/NestedLoop/MultiplicationTableEx.java
@@ -0,0 +1,29 @@
+package day6.NestedLoop;
+
+public class MultiplicationTableEx {
+
+	public static void main(String[] args) {
+		// 구구단 2단부터 9단까지 출력하는 코드를 작성하세요.
+		
+		/* 외부 반복문
+		 * 반복 횟수 : i는 2부터 9까지 1씩 증가 
+		 * 규칙성 : i단이 출력
+		 * 
+		 */
+		int i; 
+		int j = 1; //j를 초기화
+		for( i = 2 ; i <= 9 ; i++ ) {
+			//i단 출력
+			/* 내부 반복물
+			 * 반복횟수 : j는 1부터 9까지 1씩 증가
+			 * 규칙성 : i x j = i*j를 출력
+			 */
+			for( j = 1 ; j <= 9 ; j++ ) {
+				System.out.println(i + 'X' + j + "=" + i*j);
+			}
+		}
+        // 이 위치에서 i와 j이 값은 얼마? i : 10, j : 10
+		System.out.println("i : " + i + ", j :"+j);
+	}
+
+}
