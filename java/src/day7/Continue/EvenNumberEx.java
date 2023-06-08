842187ee925628cf9508cef0dcab59b0e71bea24
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-08 10:30:22 +0900

    자바 : continue예제

---
+++ java/src/day7/Continue/EvenNumberEx.java
@@ -0,0 +1,27 @@
+package day7.Continue;
+
+public class EvenNumberEx {
+
+	public static void main(String[] args) {
+		//continue문을 이용한 10이하 짝수 출력 예제
+		int i;
+		
+		for( i = 1 ; i<= 10 ; i++ ) {
+			// i가 홀수이면 건너뜀
+			if(i % 2 != 0) {
+			continue;
+			}
+			System.out.println(i);
+		}
+	    i = 0;
+	    while( ++i <= 10) {
+	    	if( i % 2 != 0){
+	    		continue;
+	    	}
+	    	System.out.println(i);
+	    	
+	}
+
+	}
+
+}
