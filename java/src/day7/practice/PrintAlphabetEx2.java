fe1b8fe11fa01a96c57c2519d29b862fb874bb8b
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-08 13:24:25 +0900

    자바 : 별 예제2 a~z를 반복되게 나열

---
+++ java/src/day7/practice/PrintAlphabetEx2.java
@@ -0,0 +1,38 @@
+package day7.practice;
+
+public class PrintAlphabetEx2 {
+
+	public static void main(String[] args) {
+	/* 다음과 같이 출력되는 코드를 작성하세요.
+	 * a
+	 * ab
+	 * abc
+	 * abcd
+	 * ...
+	 * abcd...xyz
+	 */
+		
+		
+	
+	/* 외부 반복문  
+	 * 반복 횟수 : end는 'a'부터 시작해서 'z'까지 1씩 증가
+	 * 규칙성 : 'a'부터 end까지 출력 후 엔터
+	 * 반복문 종료 후 : 없음
+	 */
+	char ch = 'a', end;
+	for ( end = 'a'; end <= 'z' ; end++ ) {
+		/* 내부 반복문
+		 * 반복 횟수 : ch는 'a'부터 시작해서 end까지 1씩 증가
+		 * 규칙성 : ch를 출력
+		 * 반복문 종료 후 : 없음
+		 */
+		
+	    for( ch = 'a' ; ch <= end ; ch++) {
+		  System.out.print(ch);
+          }
+	    System.out.println();
+
+	}
+
+	}
+}
