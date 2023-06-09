13ddc15546115b6e2fe07eb350c12f1879d1a308
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-09 15:07:51 +0900

    자바 : 월 구하기 숙제 해봄

--- java/src/day8/homework/MethodMonth.java
+++ java/src/day8/homework/MethodMonth.java
@@ -1,5 +1,7 @@
 package day8.homework;
 
+import java.util.Scanner;
+
 public class MethodMonth {
 
 	public static void main(String[] args) {
@@ -10,7 +12,46 @@ public class MethodMonth {
 		 * 3
 		 * March
 		 */
+		
+		int month;
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" input month : ");
+		month = sc.nextInt();
+		
+		
+		
+		sc.close();
 
+	}
+	
+	
+	
+	/** 월이 주어지면 주어진 월에 맞는 계절을 알려주는 메서드
+	 * 매개변수 : 월 => int month
+	 * 리턴타입 : 계절 = > Spring 
+	 * 메서드명 : getSeason
+	 * */
+	public static void printEvenNumber (int month) {
+		 switch( month ) {
+		 case 1 : month = '1';
+		 System.out.println("Jaunary");
+		 break;
+		 case 2 : month = '2';
+		 System.out.println("Febuary");
+		 break;
+		 case 3 : month = '3';
+		 System.out.println("March");
+		 break;
+		 case 4 : month = '4';
+		 System.out.println("April");
+		 break;
+		 case 5 : month = '5';
+		 System.out.println("May");
+		 break;
+		
+		 }
+		 
+		
 	}
 
 }
