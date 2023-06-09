77163f5495eb24900d590e851c4c8bc5604ee4bd
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-09 14:42:52 +0900

    자바 : day8.homework

---
+++ java/src/day8/homework/MethodMonth.java
@@ -0,0 +1,16 @@
+package day8.homework;
+
+public class MethodMonth {
+
+	public static void main(String[] args) {
+		/* 월을 입력받아 월에 맞는 영어를 출력하세요.
+		 * 단, 메서드 이용
+		 * 예 : 
+		 * input month : 
+		 * 3
+		 * March
+		 */
+
+	}
+
+}
---
+++ java/src/day8/homework/MethodPrintCharEx.java
@@ -0,0 +1,17 @@
+package day8.homework;
+
+public class MethodPrintCharEx {
+
+	public static void main(String[] args) {
+		/* 다음과 같이 출력하기 위한 코드를 작성하세요.
+		 * 단, 메서드 이용
+		 * *****
+		 * ***
+		 * ?????
+		 * ^^^^^^^
+		 * 한 줄 씩 출력하면 됨 ( 한번에 출력 X )
+		 */
+
+	}
+
+}
