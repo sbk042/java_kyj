6281216c1e05daf69f459db1f3855400430cdd47
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-16 00:16:21 +0900

    day12 로또 숙제..

--- java/src/day12/homework/LottoEx.java
+++ java/src/day12/homework/LottoEx.java
@@ -1,5 +1,7 @@
 package day12.homework;
 
+import java.util.Scanner;
+
 public class LottoEx {
 
 	public static void main(String[] args) {
@@ -16,15 +18,14 @@ public class LottoEx {
 		 * user : 1 2 3 4 5 6 
 		 * losing ticket!
 		 */
-		int lotto[] = new int[6]; // 로또를 받을 저장소 6개를 생성
 		
-		// 랜덤번호 생성
-		for( int i = 0 ; i < 6; i++) {
-			lotto[i] = (int)(Math.random()*45)+1;
-			
-			//중복 번호 제거
-			for(int j = 0 ; j<i; ) {
-			}
-		}
+		int lotto[] = new int[6]; // 로또를 랜덤으로 받을 저장소 6개를 생성
+		int user[] = new int[6]; // 사용자가 입력한 수를 저장할 배열 ( 내가 입력)
+		int min = 1, max = 45; // 로또 랜덤 수 최소값 최대값 지정
+		Scanner sc = new Scanner(System.in); //입력받을 스캐너 생성
+		
+		
+		// 1.) 로또 번호 랜덤 생성 (중복x)
+		
 	}
 }
