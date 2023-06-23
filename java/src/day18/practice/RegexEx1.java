6e80684fdf2c24aa1f2a4cafd5bb36701a2a17a3
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-23 14:53:53 +0900

    자바 : day18 Regex 예제

---
+++ java/src/day18/practice/RegexEx1.java
@@ -0,0 +1,18 @@
+package day18.practice;
+
+import java.util.regex.Pattern;
+
+public class RegexEx1 {
+
+	public static void main(String[] args) {
+	// 입력받은 문자열이 http:// 또는 https://로 
+	// 시작하는지 확인하는 정규표현식을 작성해주세요.
+		String str = "https://www.naver.com";
+		String regex = "^https?://[a-zA-Z\\.]+$";
+		boolean result = Pattern.matches(regex, str);
+		System.out.println(str + " : " + result);
+		//str에 따라 true false로 나뉘어진다.
+		
+	}
+
+}
