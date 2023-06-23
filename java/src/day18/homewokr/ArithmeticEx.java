2a3058ecceb06c0e8ac6a7ef75a5b6edb195b753
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-23 14:54:04 +0900

    자바 : day18  homework

---
+++ java/src/day18/homewokr/ArithmeticEx.java
@@ -0,0 +1,37 @@
+package day18.homewokr;
+
+public class ArithmeticEx {
+
+	public static void main(String[] args) {
+	/* 산술연산을 하는 프로그램을 작성하세요.
+	 * 인터페이스 이용
+	 * public interface Program{
+	 * 	void run();
+	 * 	void printMenu();
+	 * 	void runMenu(int menu);
+	 * 
+	 * 1. Input Integer
+	 * 2. Input Operator
+	 * 3. Exit
+	 * 
+	 * menu : 1
+	 * input : 1 2 
+	 * 1. Input Integer
+	 * 2. Input Operator
+	 * 3. Exit
+	 * 
+	 * menu : 2
+	 * input : +
+	 * result : 3
+	 * 
+	 * 1. Input Integer
+	 * 2. Input Operator
+	 * 3. Exit
+	 * 
+	 * menu : 3
+	 * Exit
+	 */
+
+	}
+
+}
---
+++ java/src/day18/homewokr/ImgEx.java
@@ -0,0 +1,18 @@
+package day18.homewokr;
+
+public class ImgEx {
+
+	public static void main(String[] args) {
+	/* 파일명을 입려받아 파일이 이미지인지 확인하는 코드를 작성하세요.
+	 * 이미지 확장자 : jpg, bmp,png(더 있지만 간단하게 3개만 체크)
+	 * input : dog.jpg
+	 * dog.jpg : Image
+	 * 
+	 * input : dog.exe
+	 * dog.exe : Not image
+	 * 
+	 */
+
+	}
+
+}
