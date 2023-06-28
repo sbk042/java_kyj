990ac8cd1f7f290f12bfc822b248a684f3d57e42
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-28 14:43:56 +0900

    자바 day21 homework

---
+++ java/src/day21/homework/updown/Updown.java
@@ -0,0 +1,12 @@
+package day21.homework.updown;
+
+public class Updown {
+
+	public static void main(String[] args) {
+		//UpDown 프로그램에서 기록을 최대 상위 5개까지 관리하도록 수정하세요.
+		/*게임 한 판이 끝났을 때 기록이 상위 기록에 해당하면 아이디 입력 후
+		기록하고, 상위 기록이 아니면 아이디를 입력하지 않음 */
+
+	}
+
+}
