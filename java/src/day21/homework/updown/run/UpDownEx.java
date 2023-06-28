eb97054f76fb34ef936b6bee387f71cdda261b86
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-29 00:43:53 +0900

    자바 : day21 homework

---
+++ java/src/day21/homework/updown/run/UpDownEx.java
@@ -0,0 +1,13 @@
+package day21.homework.updown.run;
+
+import day21.practice.updown.controller.GameManager;
+
+public class UpDownEx {
+
+	public static void main(String[] args) {
+		GameManager gm = new GameManager();
+		gm.run();
+
+	}
+
+}
---
+++ java/src/day21/homework/updown/vo/RecordGame.java
@@ -0,0 +1,30 @@
+package day21.homework.updown.vo;
+
+import lombok.Data;
+
+@Data
+//sort에 null을 넣어도 가능하게 하기 위해 Comparable을 구현한 구현클래스로 만듬.
+public class RecordGame implements Comparable<RecordGame> {
+		
+	
+	private String id;
+	private int count;
+	
+	public RecordGame(String id, int count) {
+		this.id = id;
+		this.count = count;
+	}
+	@Override
+	public String toString {
+		return "[" + id + " : " + count + "]";
+	}
+	@Override
+	public int compare(RecordGame o1, RecordGame o2) {
+		return o1.getCount()- o2.getCount();
+	}
+	@Override
+	public int compareTo(RecordGame o) {
+		return count - o.getCount();
+	}
+	
+}
