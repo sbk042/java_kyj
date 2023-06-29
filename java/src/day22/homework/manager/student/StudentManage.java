da0390443fb710ff5ac313a38f9df033b525e92b
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-06-29 14:09:35 +0900

    자바 : day22 homework

---
+++ java/src/day22/homework/manager/student/StudentManage.java
@@ -0,0 +1,14 @@
+package day22.homework.manager.student;
+
+public class StudentManage {
+
+	public static void main(String[] args) {
+		//전체적으로 학생들을 관리하는 프로그램을 만들어라.
+		/* - 학생 관리(입학, 전학(오는 학생, 가는 학생),졸업, 진학)
+		 * - 성적 관리
+		 * - 면담
+		 * */
+
+	}
+
+}
