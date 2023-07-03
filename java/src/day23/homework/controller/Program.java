2be9f5e352d19f64f3c95d13248475598e2f8e93
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-07-03 11:11:08 +0900

    자바 : day23  homework 전화번호부 관리

---
+++ java/src/day23/homework/controller/Program.java
@@ -0,0 +1,9 @@
+package day23.homework.controller;
+
+public interface Program {
+	void printMenu();
+	void run();
+	void runMenu(int menu);
+	void load();
+	void save();
+}
---
+++ java/src/day23/homework/run/PhoneListEx.java
@@ -0,0 +1,13 @@
+package day23.homework.run;
+
+import day23.homework.controller.PhoneManager;
+
+public class PhoneListEx {
+
+	public static void main(String[] args) {
+		PhoneManager pm = new PhoneManager();
+		pm.run();
+
+	}
+
+}
