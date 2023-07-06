a200ca14d90886122643c158c7e58ebf45288955
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-07-06 16:45:05 +0900

    자바 :수정내용

--- java/src/day24/network/ClientEx3.java
+++ java/src/day24/network/ClientEx3.java
@@ -3,6 +3,7 @@ package day24.network;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
@@ -12,6 +13,7 @@ public class ClientEx3 {
 	public static void main(String[] args) {
 		int port = 5001;//1.포트번호 설정
 		String ip = "192.168.30.199";//1-1. 서버 IP설정
+		//ip = InetAddress.getLocalHost().getHostAddress(); //이렇게 입력하면 안됨
 		Socket socket = new Socket();//2. 소켓 생성
 		try{
 			//3. 서버에게(IP,port) 연결 요청
