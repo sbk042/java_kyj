8b72b60878e36d156f9fde72eadff5f5434221e2
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-08-09 17:36:13 +0900

    DB : day05_22.영화 예매사이트_조회예제

---
+++ DB/day04/22.영화 예매사이트_조회예제.sql
@@ -0,0 +1,90 @@
+-- 1.) 영화 오펜하이머에 출연한 감독과 배우들을 조회하는 쿼리
+SELECT 
+	MO_TITLE AS '영화제목', 
+	FP_NAME AS '영화인',
+    RO_ROLE AS '역할'
+FROM 
+	FILM_PERSON
+		JOIN
+	ROLE ON FP_NUM = RO_FP_NUM
+		JOIN
+	MOVIE ON MO_NUM = RO_MO_NUM
+WHERE
+	MO_TITLE LIKE '오펜하이머';
+
+-- 영화 오펜하이머 기본 정보를 조회하는 쿼리(제목, 제목(영문), 개봉일, 런닝타임, 줄거리, 연령제한, 예매율)
+SELECT 
+    MO_TITLE AS 제목,
+    MO_TITLE_ENG AS '제목(영문)',
+    MO_OPENING_DATE AS 개봉일,
+    MO_RUNNING_TIME AS 런닝타임,
+    MO_PLOT AS 줄거리,
+    MO_AG_NAME AS 연령제한,
+    CONCAT(MO_RESERVATION_RATE, '%') AS 예매율
+FROM
+    MOVIE
+WHERE
+    MO_TITLE = '오펜하이머';
+
+-- 영화 오펜하이머의 트레일러와 스틸컷 파일의 개수를 조회하는 쿼리
+SELECT 
+	MO_TITLE AS '영화제목',
+    FI_STATE AS '타입',
+    COUNT(*) AS '개수'
+FROM 
+	FILE 
+		JOIN 
+	MOVIE_FILE ON MF_FI_NUM = FI_NUM
+		JOIN 
+    MOVIE ON MF_MO_NUM = MO_NUM
+WHERE
+	MO_TITLE = '오펜하이머'
+ GROUP BY FI_STATE;
+
+-- abc회원의 예매 내역을 조회하는 쿼리(영화명, 시간, 좌석명)
+SELECT
+	MO_TITLE AS 영화제목, 
+    MS_START_TIME AS 상영시간, 
+    SE_NAME AS 좌석
+FROM 
+	RESERVATION
+		JOIN 
+    RESERVATION_LIST ON RL_RV_NUM = RV_NUM
+		JOIN
+	SEAT ON RL_SE_NUM = SE_NUM
+		JOIN
+	MOVIE_SCHEDULE ON MS_NUM = RV_MS_NUM
+		JOIN
+	MOVIE ON MO_NUM = MS_MO_NUM
+WHERE RV_ME_ID = 'abc123';
+
+-- abc회원의 예매 내역을 조회하는 쿼리(영화명, 시간, 성인x명, 청소년 x명)
+SELECT
+	MO_TITLE AS 영화제목, 
+    MS_START_TIME AS 상영시간, 
+    SUM(RV_ADULT) AS 성인수, SUM(RV_TEENAGER) AS 청소년수
+FROM 
+	RESERVATION
+		JOIN
+	MOVIE_SCHEDULE ON MS_NUM = RV_MS_NUM
+		JOIN
+	MOVIE ON MO_NUM = MS_MO_NUM
+WHERE RV_ME_ID = 'abc123'
+GROUP BY RV_NUM;
+
+-- CGV강남 영화관에서 콘크리트 유토피아 20:50에 예매 가능한 좌석을 조회하는 쿼리(OUTER JOIN)
+SELECT
+   SE_NAME
+FROM 
+	SEAT 
+		JOIN
+    SCREEN ON SE_SC_NUM = SC_NUM
+		JOIN
+    MOVIE_SCHEDULE ON MS_SC_NUM = SC_NUM
+		
+WHERE
+	MS_NUM = 8 AND SE_NUM NOT IN(
+	SELECT RL_SE_NUM FROM RESERVATION
+		JOIN RESERVATION_LIST ON RL_RV_NUM = RV_NUM
+		WHERE RV_MS_NUM = 8
+    );
