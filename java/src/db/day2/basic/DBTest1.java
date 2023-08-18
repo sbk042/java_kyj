d4c8351f8a6e40ac7286acfe7baef488cd8b6055
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-08-18 17:07:07 +0900

    JAVA : DB연동 복습

---
+++ java/src/db/day2/basic/DBTest1.java
@@ -0,0 +1,107 @@
+package db.day2.basic;
+
+import java.sql.*;
+import java.util.*;
+
+public class DBTest1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Connection con = null;
+		
+		String url = "jdbc:mysql://localhost:3306/text";
+		System.out.print("DB 계정 : ");
+		String dbId = sc.next();
+		System.out.print("DB 비번 : ");
+		String dbPw = sc.next();
+		
+		try {
+			con = DriverManager.getConnection(url, dbId, dbPw);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		// Run -> Run Configuration -> common -> MS949 
+		System.out.println("연결 성공!");
+		
+		/*
+		String sql = "insert into member(me_id, me_pw) values(?,?)";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			// 회원가입한 아이디와 비번을 입력
+			System.out.println("ID : ");
+			String id = sc.next();
+			System.out.println("PW : ");
+			String pw = sc.next();
+			// 쿼리에 있는 ? 대신 입력한 아이디와 비번을 넣어줌. 쿼리를 준비
+			// setXXX(index, value) : index는 ?의 위치로 1부터 시작, value는 ? 대신 들어갈 값
+			ps.setString(1, id); // 문자면 setString 정수면 setInt
+			ps.setString(2, pw); // ?가 있는 만큼 setXXX이 나와야됨
+			
+			// 쿼리를 실행
+			int resultCount = ps.executeUpdate();
+			if(resultCount != 0 ) {
+				System.out.println("회원가입 성공!");
+			}else {
+				System.out.println("회원가입 실패!");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		*/
+		/*
+		// 업데이트문 쿼리 만들기
+		String sql = "update member set me_board_count = ? where me_id = ?";
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			// ?에 들어갈 값을 설정
+			System.out.print("ID : ");
+			String id = sc.next();
+			ps.setString(2, id);
+
+			System.out.print("정수 : ");
+			int count = sc.nextInt();
+			ps.setInt(1, count);
+			// 쿼리를 실행
+			int resultCount = ps.executeUpdate();
+			if(resultCount != 0) {
+				System.out.println("수정 성공");
+			}else {
+				System.out.println("수정 실패");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		*/
+		// 검색결과를 조회
+		String sql = "select me_id, me_pw, me_board_count from member";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(sql);
+			// ?설정 => ?가 없어서 생략
+			
+			// 쿼리를 실행
+			ResultSet resultSet = ps.executeQuery();
+			// select문은 반복문을 통해서 값을 읽어옴
+			while(resultSet.next()) {
+				String id = resultSet.getString(1);
+				String pw = resultSet.getString(2);
+				int boardCount = resultSet.getInt(3);
+				System.out.println(id + " : " + pw + " : " + boardCount);
+			}
+	
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		
+		try {
+			// 연결한 connection을 닫아줌
+			if(con != null && !con.isClosed()) { //isClosed = 닫혀있는지
+				con.close(); // 먼저 null이 아닌지 확인하고 닫혀 있지 않으면 닫아주세요. 
+			}
+			sc.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
