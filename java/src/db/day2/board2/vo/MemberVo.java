e33f4febf4fbeee791f32c8713e9e2a2c5888b04
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-08-18 17:16:57 +0900

    JAVA:게시글 관리 프로그램-MyBatis이용(VO-BoardVO,MemberVo)

---
+++ java/src/db/day2/board2/vo/MemberVo.java
@@ -0,0 +1,12 @@
+package db.day2.board2.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MemberVo {
+	private String me_id;
+	private String me_pw;
+	private int me_board_count;
+}
