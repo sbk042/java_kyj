3dae305ff1b51573f9100df7f7b6c7ab74ed5654
sbk042 <134986683+sbk042@users.noreply.github.com>
2023-09-18 10:28:52 +0900

    SPRING 복습2 : 회원가입비밀번호 암호화적용

--- spring_projects/edu/src/main/java/kr/kh/edu/service/MemberServiceImp.java
+++ spring_projects/edu/src/main/java/kr/kh/edu/service/MemberServiceImp.java
@@ -3,6 +3,7 @@ package kr.kh.edu.service;
 import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.kh.edu.dao.MemberDAO;
@@ -13,6 +14,10 @@ public class MemberServiceImp implements MemberService{
 	
 	@Autowired
 	MemberDAO memberDao;
+	
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
 	@Override
 	public boolean signup(MemberVO member) {
 		// 넘겨진 데이터가 
@@ -32,6 +37,15 @@ public class MemberServiceImp implements MemberService{
 			return false;
 		}
 		// 이메일 체크는 생략하겠음
+		
+		// 비밀번호 암호화시작
+		// 화면에서 입력받은 비밀번호를 암호화 하고
+		String encodedPassword = passwordEncoder.encode(member.getMe_pw());
+		// 암호화 된 비밀번호를 회원정보에 수정해준다.
+		member.setMe_pw(encodedPassword);
+		
+		
+		
 		return memberDao.insertMember(member);
 	}
 }
